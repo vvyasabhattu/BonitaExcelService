1cfa5b4571e3c281a5b6d6abcdcc36711c7df3ab
gpamu <gpamu@GPAMU-L-1277.evoketechnologies.com>
2021-02-05 14:53:22 +0530

    Modified Db

--- src/main/java/com/evoke/bonita/controller/ExcelProcessController.java
+++ src/main/java/com/evoke/bonita/controller/ExcelProcessController.java
@@ -1,5 +1,14 @@
 package com.evoke.bonita.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ExcelProcessController {
 
+	
+	public void insertUSMFUGData() {
+		
+		
+	}
+	
 }
